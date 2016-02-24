834846daa9b78ba4d0c164b99ee639fd561ecf03
siyounguk <si.younguk@gmail.com>
2016-02-24 16:17:45 +0000

    created city service, with watcher to bind city between controllers

--- app.js
+++ app.js
@@ -18,11 +18,29 @@ weatherApp.config(function($routeProvider){
 })
 
 
-weatherApp.controller('homeController', ['$scope', function($scope){
+weatherApp.service('cityService', function(){
+
+    var self = this;
+
+    self.city = "New York, NY";
+
+});
+
+
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
+  $scope.city = cityService.city;
+
+  $scope.$watch('city', function() {
+      cityService.city = $scope.city;
+  });
 
 }]);
 
 
-weatherApp.controller('forecastController', ['$scope', function($scope){
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
+  $scope.city = cityService.city;
 
+  $scope.$watch('city', function() {
+      cityService.city = $scope.city;
+  });
 }]);

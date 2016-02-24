1540286ed10612d9a78aede27091305cd3a86e0a
siyounguk <si.younguk@gmail.com>
2016-02-24 17:21:20 +0000

    added back to search link

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParam
       cityService.city = $scope.city;
   });
 
-  $scope.days = $routeParams.days || 2;
+  $scope.days = $routeParams.days || "2";
 
   $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=44db6a862fba0b067b1930da0d769e98",{
     callback: 'JSON_CALLBACK'}, {get: {method: 'JSONP'}
